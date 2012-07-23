d9e6f8a54734230faeb0d6e9a72ff6ea9ec78203
Allard van Helbergen <allard@vanhelbergen.com>
2012-07-23 22:30:21 +0100

    changed agenda content

--- protected/views/site/projecten.php
+++ protected/views/site/projecten.php
@@ -15,94 +15,96 @@
     <div class="tab-pane active" id="projecten">
       <h2>Huidige Projecten</h2>
       <div itemscope itemtype="http://schema.org/Event">
-        <h3 itemprop="name">Requiem, G. Faur&eacute;</h3>
-        <p itemprop="description">
-          In samenwerking met het
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/MusicGroup">
-            <span itemprop="name">Noordhollands Muziekensemble</span>
-          </span>
-          o.l.v.
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Hendrik Timmerman</span>.
-          </span>
-        </p>
-        <p>
-          <span itemprop="location"
-              itemscope
-              itemtype="http://schema.org/Church">
-            <span itemprop="name">Ter Coulsterkerk</span>,
-            <span itemprop="address"
-                itemscope
-                itemtype="http://schema.org/PostalAddress">
-              <span itemprop="streetAddress">Holleweg 111</span>,
-              <span itemprop="addressLocality">Heiloo</span>
-            </span>
-          </span>
-          <br/>
-          <span itemprop="startDate" content="2012-05-17T20:00">
-            17 mei 2012, Aanvang: 20.00
-          </span>
-          <br/>
-          <a href="http://nhme.nl/agenda.html">Programma van NHME</a>
-        </p>
-      </div>
-      <div itemscope itemtype="http://schema.org/Event">
-        <h3 itemprop="name">Dido and Aeneas, H. Purcell</h3>
-        <p itemprop="description">
-          In samenwerking met
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/MusicGroup">
-            <span itemprop="name">Koor de Stemming</span>
-          </span>
-          o.l.v.
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Joost Schouten</span>.
-          </span>
-          <br/>
-          Rol: Heks 1
-        </p>
-        <p>
-          <span itemprop="startDate" content="2020-01-01">
-            Informatie over locatie en tijd volgt
-          </span>
-          <br />
-          <a href="http://www.koordestemming.nl/">
-              Website van De Stemming</a>
-        </p>
-      </div>
-      <div itemscope itemtype="http://schema.org/Event">
         <h3 itemprop="name">Jamaica Farewell</h3>
-        <p itemprop="description">
-          Solo muziektheater voorstelling in samenwerking met schrijver
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Marjolijn van Heemstra</span>,
-          </span> regisseur
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Luc Boyer</span>
-          </span>
-          en instrumentalisten
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Boele Weemhoff</span>
-          </span> en
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Martijn Heida</span>.
-          </span>
-        </p>
+        <br>
+        <div itemprop="description">
+          <blockquote class="">
+            <p>
+              Down the way where the nights are gay<br>
+              And the sun shines daily on the mountain top,
+            </p>
+            <p>
+              I took a trip on a sailing ship<br>
+              And when I reached Jamaica I made a stop. 
+            </p>
+            <p>
+              But I'm sad to say, I'm on my way,<br>
+              Won't be back for many a day...
+            </p>
+             <small><cite>Harry Belafonte   in Jamaica Farewell</cite></small>
+          </blockquote>
+          <p>
+            Wat is dat thuisland waar Belafonte over zingt? En hoe komt het dat
+            zijn verlangen zo herkenbaar is? Is de zoektocht naar het thuisland
+            universeel en van alle tijden? Of is deze verbonden aan de
+            globalisatie waar wij vandaag de dag deel van uitmaken? Hoe zoekt
+            de ontheemde mens anno 2012 naar de plek waar hij thuis hoort? En
+            is deze plek überhaupt bereikbaar, of is het thuisland een utopie?  
+          </p>
+          <p>
+            Jet van Helbergen maakt in samenwerking met schrijfster Marjolijn
+            van Heemstra een voorstelling over het grotere verhaal achter dit
+            liedje. Een voorstelling over heimwee en verlangen, in alle soorten
+            en maten. Met bestaande en nieuw gecomponeerde muziek. Locatie is
+            de stille Palmenkas in de Amsterdamse Hortus, vol bomen die
+            afkomstig zijn uit andere thuislanden en op hun eigen manier
+            verstekelingen.
+          </p>
+          <dl class="dl-horizontal">
+            <dt>Zang en spel:</dt>
+            <dd itemprop="performer"
+                itemscope
+                itemtype="http://schema.org/Person">
+              <span itemprop="name">Jet van Helbergen</span>
+            </dd>
+            <dt>Tekst:</dt>
+            <dd itemprop="performer"
+                itemscope
+                itemtype="http://schema.org/Person">
+              <span itemprop="name">Marjolijn van Heemstra</span>
+            </dd>
+            <dt>Instrumentalisten:</dt>
+            <dd><span itemprop="performer"
+                itemscope
+                itemtype="http://schema.org/Person">
+              <span itemprop="name">Boele Weemhoff</span>
+            </span> en
+            <span itemprop="performer"
+                itemscope
+                itemtype="http://schema.org/Person">
+              <span itemprop="name">Martijn Heida</span>
+            </span>
+            <dt>Composities:</dt>
+            <dd>
+              <span itemprop="performer"
+                  itemscope
+                  itemtype="http://schema.org/Person">
+                <span itemprop="name">Roald van Oosten</span>,
+              </span>
+              <span itemprop="performer"
+                  itemscope
+                  itemtype="http://schema.org/Person">
+                <span itemprop="name">Joost Schouten</span>,
+              </span>
+              <span itemprop="performer"
+                  itemscope
+                  itemtype="http://schema.org/Person">
+                <span itemprop="name">Boele Weemhoff</span>
+              </span> en
+              <span itemprop="performer"
+                  itemscope
+                  itemtype="http://schema.org/Person">
+                <span itemprop="name">Martijn Heida</span>
+              </span>
+            </dd>
+            <dt>Eindregie:</dt>
+            <dd itemprop="performer"
+                itemscope
+                itemtype="http://schema.org/Person">
+              <span itemprop="name">Bart Kiene</span>
+            </dd>
+          </dl>
+        </div>
         <p>
           <span itemprop="location"
               itemscope
@@ -119,56 +121,57 @@
             17 september 2012, Aanvang: 20.00
           </span>
           (try-out)<br>
-          19 september 2012, Aanvang: 20.00<br>
+          19 september 2012, Aanvang: 20.00 (premiere)<br>
           20 september 2012, Aanvang: 20.00<br>
           21 september 2012, Aanvang: 20.00<br>
           23 september 2012, Aanvang: 17.15<br>
-          26 september 2012, Aanvang: 20.00<br>
-          27 september 2012, Aanvang: 20.00<br>
+          26 september 2012, Aanvang: 20.00 (uitverkocht)<br>
+          27 september 2012, Aanvang: 20.00 (uitverkocht)<br>
           28 september 2012, Aanvang: 20.00<br>
           30 september 2012, Aanvang: 17.15<br>
           <br>
-          <a href="http://dehortus.nl/programma">
-            Programma van De Hortus</a><br>
+          <a href="http://dehortus.nl/programma/#1104">
+            Kaartverkoop en informatie</a>
+          <br>
           <a href="http://www.voordekunst.nl/vdk/project/view/430-jamaica-farewell">
             Omschrijving op Voor de Kunst</a>
         </p>
       </div>
-      <div itemscope itemtype="http://schema.org/Event">
-        <h3 itemprop="name">Weihnachtsoratorium, J.S. Bach</h3>
-        <p itemprop="description">
-          In samenwerking met het 
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/MusicGroup">
-            <span itemprop="name">Noordhollands Muziekensemble</span>
-          </span>
-          o.l.v.
-          <span itemprop="performer"
-              itemscope
-              itemtype="http://schema.org/Person">
-            <span itemprop="name">Hendrik Timmerman</span>.
-          </span>
-        </p>
-        <p>
-          <span itemprop="location"
-              itemscope
-              itemtype="http://schema.org/Church">
-            <span itemprop="name">Grote- of St. Laurenskerk</span>,
-            <span itemprop="address"
-                itemscope
-                itemtype="http://schema.org/PostalAddress">
-              <span itemprop="addressLocality">Alkmaar</span>
-            </span>
-          </span>
-          <br>
-          <span itemprop="startDate" content="2012-12-15T20:00">
-            15 december 2012, Aanvang: 20.00
-          </span>
-          <br>
-          <a href="http://nhme.nl/agenda.html">Programma van NHME</a>
-        </p>
-      </div>
+      <div itemscope itemtype="http://schema.org/Event">
+        <h3 itemprop="name">Requiem, W.A. Mozart</h3>
+        <p itemprop="description">
+          In samenwerking met het
+          <span itemprop="performer"
+              itemscope
+              itemtype="http://schema.org/MusicGroup">
+            <span itemprop="name">Noordhollands Muziekensemble</span>
+          </span>
+          o.l.v.
+          <span itemprop="performer"
+              itemscope
+              itemtype="http://schema.org/Person">
+            <span itemprop="name">Hendrik Timmerman</span>.
+          </span>
+        </p>
+        <p>
+          <span itemprop="location"
+              itemscope
+              itemtype="http://schema.org/Church">
+            <span itemprop="name">Grote- of St. Laurenskerk</span>,
+            <span itemprop="address"
+                itemscope
+                itemtype="http://schema.org/PostalAddress">
+              <span itemprop="addressLocality">Alkmaar</span>
+            </span>
+          </span>
+          <br/>
+          <span itemprop="startDate" content="2012-10-20T20:00">
+            20 oktober 2012, Aanvang: 20.00
+          </span>
+          <br/>
+          <a href="http://nhme.nl/">Kaartverkoop en informatie</a>
+        </p>
+      </div>
       <div itemscope itemtype="http://schema.org/Event">
         <h3 itemprop="name">Besloten Kerst-recital</h3>
         <p itemprop="description">
@@ -193,12 +196,47 @@
             </span>
           </span>
           <br>
-          <span itemprop="startDate" content="2012-12-20">
-            20 december 2012
+          <span itemprop="startDate" content="2012-12-17">
+            17 - 24 december 2012
           </span>
           <br>
           <a href="http://cyrillibrahim.wordpress.com/">
             Website van Cyrill Ibrahim</a>
+        </p>
+      </div>
+      <div itemscope itemtype="http://schema.org/Event">
+        <h3 itemprop="name">Weihnachtsoratorium, J.S. Bach</h3>
+        <p itemprop="description">
+          In samenwerking met het 
+          <span itemprop="performer"
+              itemscope
+              itemtype="http://schema.org/MusicGroup">
+            <span itemprop="name">Noordhollands Muziekensemble</span>
+          </span>
+          o.l.v.
+          <span itemprop="performer"
+              itemscope
+              itemtype="http://schema.org/Person">
+            <span itemprop="name">Hendrik Timmerman</span>.
+          </span>
+        </p>
+        <p>
+          <span itemprop="location"
+              itemscope
+              itemtype="http://schema.org/Church">
+            <span itemprop="name">Ter Coulskerk</span>,
+            <span itemprop="address"
+                itemscope
+                itemtype="http://schema.org/PostalAddress">
+              <span itemprop="addressLocality">Heiloo</span>
+            </span>
+          </span>
+          <br>
+          <span itemprop="startDate" content="2012-12-22T20:00">
+            22 december 2012, Aanvang: 20.00
+          </span>
+          <br>
+          <a href="http://nhme.nl/">Kaartverkoop en informatie</a>
         </p>
       </div>
     </div>
